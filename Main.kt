181b6cc82ed03d018bb0e1611749d0ead6065a56
Mohsen Dehbashi <dehbashi@soc1.ir>
2019-07-20 09:34:39 +0430

    !

--- Main.kt
+++ Main.kt
@@ -2,6 +2,6 @@ package robotopia
 
 fun main() {
     println("Hello, Git!")
-    println("Mohsen is printing ${2 * 3}.")
+    println("Mohsen is printing ${2 * 3}")
     println("First conflict!")
 }
