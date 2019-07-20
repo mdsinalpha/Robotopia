c418d9fd9c2b088f7fdbdf6c41136d2dc07f5a7a
Mohamadhassan <emohamadhassan@gmail.com>
2019-07-20 17:31:41 +0430

    Can't remember changes...

--- models/structures.kt
+++ models/structures.kt
@@ -1,14 +1,16 @@
 package robotopia.models
 
-sealed class Structure{
+sealed class Structure {
 
     abstract val cost: Long
 }
 
-open class HumanStructure(val head: Head, val body: Body,
-                          val eye: Eye, val assistant: VoiceAssistant,
-                          val leftArm: Arm, val rightArm: Arm,
-                          val leg: Leg): Structure(){
+open class HumanStructure(
+    val head: Head, val body: Body,
+    val eye: Eye, val assistant: VoiceAssistant,
+    val leftArm: Arm, val rightArm: Arm,
+    val leg: Leg
+) : Structure() {
     init {
         head.takeIf { it.model in listOf(Head.Model.Skeletonic, Head.Model.Elemental) }
             ?: throw IllegalArgumentException("$head is not compatible with a HumanStructure.")
@@ -18,12 +20,15 @@ open class HumanStructure(val head: Head, val body: Body,
         get() = 1
 }
 
-open class CatStructure: Structure()
+open class CatStructure : Structure()
 
-open class DogStructure: Structure()
+open class DogStructure : Structure()
 
-open class SquirrelStructure: Structure()
+open class SquirrelStructure : Structure()
 
-open class BirdStructure: Structure()
+open class BirdStructure : Structure()
 
-open class BearStructure: Structure()
+open class BearStructure(
+    val head: Head, val body: Body, val eye: List<Eye>, val assistant: VoiceAssistant
+    val arms: List<Arm>, val legs: List<Leg>
+) : Structure()
